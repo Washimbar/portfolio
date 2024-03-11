171c141f7de0e79e10371e603b66615393487881
WashimBar <washimbari0001@gmail.com>
2024-03-12 05:28:34 +0530

    sidebar added

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import Navbar from "./navbar/Navbar";
 import Footer from "./footer/Footer";
 import "./Layout.css";
+import Sidebar from "./sidebar/Sidebar";
 
 const Layout = ({ children }) => {
 	return (
 		<div className="layout">
 			<Navbar />
-			<main className="content">{children}</main>
+			<div className="container">
+        <aside className="sidebar">
+          <Sidebar />					
+				</aside>
+				<main className="content">{children}</main>
+			</div>
 			<Footer />
 		</div>
 	);
---
+++ src/layout/sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+		<div>
+			<ul>
+				<li>Link 1</li>
+				<li>Link 2</li>
+				<li>Link 3</li>
+			</ul>
+		</div>
+	);
+}
+
+export default Sidebar
